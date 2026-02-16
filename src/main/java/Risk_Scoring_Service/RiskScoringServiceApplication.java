5e66b96de9b530e373f57f05dc7c14da824b3a9d
Vinicius Henrique <vinciusssvl9@gmail.com>
2026-02-15 21:44:53 -0300

    feature: initial config

---
+++ src/main/java/Risk_Scoring_Service/RiskScoringServiceApplication.java
@@ -0,0 +1,13 @@
+package Risk_Scoring_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskScoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskScoringServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Risk_Scoring_Service/RiskScoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Risk_Scoring_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskScoringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
